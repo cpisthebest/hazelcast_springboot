664ac1241db850967aba32f2c391e9198aef425c
Alpana <alpana.p@idigicloudtech.com>
2025-12-22 07:01:15 +0530

    hazelcast - send parameter to root target method

--- src/main/java/com/example/cache/service/BookService.java
+++ src/main/java/com/example/cache/service/BookService.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class BookService {
 //    @Cacheable(value="books" , key="#isbn")
-    @Cacheable(value="books" , key="#root.target.getKey() + #isbn")
+    @Cacheable(value="books" , key="#root.target.getKey(#isbn) + #isbn")
     public String getBooksByIsbn(String isbn){
         try{
             TimeUnit.SECONDS.sleep(6);
@@ -18,7 +18,8 @@ public class BookService {
         }
         return "ISBN -> "+isbn;
     }
-    public String getKey(){
-        return "1_";
+    public String getKey(String isbn){
+        System.out.println(isbn);
+        return "1_"+isbn;
     }
 }
